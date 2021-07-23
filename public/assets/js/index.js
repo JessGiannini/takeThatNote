459c13e77beafdd53ddd5ca401d2a2d3d2bf19b4
JessGiannini <jessgiannini@gmail.com>
2021-07-22 20:57:30 -0700

    figuring out routes

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -121,7 +121,7 @@ const handleRenderSaveBtn = () => {
 // Render the list of note titles
 const renderNoteList = async (notes) => {
   let jsonNotes = await notes.json();
-  console.log("notes------", jsonNotes);
+  console.log("hello notes", jsonNotes);
   if (window.location.pathname === "/notes") {
     noteList.forEach((el) => (el.innerHTML = ""));
   }
